8fffccfe1d83fc596d2fd7c6a9d893b206c38910
Me-Suspiciuos <fherrera2016@gmail.com>
2016-12-27 20:18:42 +0000

    Simple module test

---
+++ first_module.py
@@ -0,0 +1,6 @@
+# Module, this function that will be use by the main project file
+
+
+def print_values():
+    print('This is an example on how use a function from other file')
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import first_module
+import random
+
+first_module.print_values()
+
+x = random.randrange(1, 100)
+print(x)
